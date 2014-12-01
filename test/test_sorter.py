7db8fd9dcee964e5296170c4ff7cb184f08e959c
Charlie Brown <stieizc@gmail.com>
2014-12-01 15:00:06 +0800

    add sorter test

---
+++ test/test_sorter.py
@@ -0,0 +1,20 @@
+import unittest
+
+from libbta.analysis.sorter import Sorter
+
+
+class SorterTestCase(unittest.TestCase):
+    def setUp(self):
+        self.args = [('qemu_virtio_req', 'qemu_virtio', {'host': 'debc',
+                      'domain': 'qemu', 'type': 'virtio'}),
+                     ('qemu_raw_req', 'qemu_virtio', {'host': 'debc',
+                      'domain': 'qemu', 'type': 'raw'})]
+        self.sorter = Sorter(self.args)
+
+    def test_name(self):
+        for layer, arg in zip(self.sorter.layers, self.args):
+            self.assertEqual(layer.name, arg[0], 'Layer Name Failed')
+
+    def test_id(self):
+        for _id, arg in zip(self.sorter.ids, self.args):
+            self.assertEqual(_id, arg[2], 'Layer Identifier Failed')
