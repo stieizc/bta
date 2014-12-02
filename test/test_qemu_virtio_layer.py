820382f916975ceff33f3833020a0d523d5cea4f
Charlie Brown <stieizc@gmail.com>
2014-12-02 14:19:19 +0800

    test qemu_virtio_layer

---
+++ test/test_qemu_virtio_layer.py
@@ -0,0 +1,18 @@
+import unittest, os
+
+from libbta.parser import parser_babeltrace as traceparser
+from libbta.analysis.qemu_virtio_layer import QemuVirtioLayer
+
+class QemuVirtioLayerTestCase(unittest.TestCase):
+    def setUp(self):
+        self.infile = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                   'babeltrace_example.txt')
+        self.events = traceparser.parse(self.infile)
+        self.layer = QemuVirtioLayer('QemuVirtioLayer')
+        for e in self.events:
+            self.layer.read_event(e)
+
+    def test_print(self):
+        #self.assertEqual(str(self.req), 'req: ')
+        print()
+        print(self.layer)
