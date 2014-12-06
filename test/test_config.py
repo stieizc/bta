9c4c9aae3e2048257e0b4c0b1c3a9940d22da2bd
Charlie Brown <stieizc@gmail.com>
2014-12-06 21:39:17 +0800

    test config

---
+++ test/test_config.py
@@ -0,0 +1,14 @@
+import unittest, os
+
+from libbta.config.configparser import ConfigParser
+
+
+class ConfigReaderTestCase(unittest.TestCase):
+    def setUp(self):
+        self.conffile = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                   'bta.yaml')
+        self.parser = ConfigParser(self.conffile)
+
+    def test_print(self):
+        print()
+        print(self.parser)
