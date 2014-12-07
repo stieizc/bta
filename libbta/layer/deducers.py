4754fc68d755858e495bf4e5cf90547e90084a59
Charlie Brown <stieizc@gmail.com>
2014-12-07 09:53:16 +0800

    VirtioRaw: fix deduce_finish

--- libbta/layer/deducers.py
+++ libbta/layer/deducers.py
@@ -6,17 +6,23 @@ def _deduce_lower_add(self, lower_req):
     When a req is added to lower, find reqs contained by lower_req, set links
     between them
     """
+    #print("Deducing on lower {0}".format(str(lower_req)))
     for upper_req in self.upper.submitted_reqs:
         if upper_req.type == lower_req.type and lower_req.contains(upper_req):
             upper_req.add_lower_req(lower_req)
+            lower_req.add_upper_req(upper_req)
+    #print("Deduced")
 
 def _deduce_upper_finish(self, upper_req):
     """
     When a req is finished in upper, find reqs linked in lower_layer, set them
-    as finished
+    as finished, if previously not finished
     """
+    #print("Deducing on upper {0}".format(str(upper_req)))
     for lower_req in upper_req.lower_reqs:
-        self.layers['lower'].finish_request(lower_req, upper_req.finish_time)
+        if not lower_req.get('finish_time'):
+            self.layers['lower'].finish_request(lower_req, upper_req.finish_time)
+    #print("Deduced")
 
 class VirtioRawDeducer(Deducer):
     """
