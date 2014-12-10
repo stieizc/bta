520694060592522798865959b25289c95f593842
Charlie Brown <stieizc@gmail.com>
2014-12-10 20:05:55 +0800

    try to improve merge_events: no need

--- libbta/parser/__init__.py
+++ libbta/parser/__init__.py
@@ -1,7 +1,6 @@
 import os
 import importlib
 
-import libbta
 
 def parse_dir(_dir):
     event_queues = []
@@ -14,10 +13,21 @@ def parse_dir(_dir):
         event_queues.append(mod.parse(_file))
     return merge_events(event_queues)
 
+
 def merge_events(event_queues):
     events = []
+
     for es in event_queues:
         events.extend(es)
     if len(event_queues) > 1:
         events.sort(key=lambda e: e.timestamp)
+    #event_queues = [es for es in event_queues if es]
+    #while len(event_queues) > 1:
+    #    heads = [es[0] for es in event_queues]
+    #    idx, min_val = min(enumerate(heads), key=lambda p: p[1].timestamp)
+    #    events.append(min_val)
+    #    event_queues[idx].popleft()
+    #    event_queues = [es for es in event_queues if es]
+    #if len(event_queues) == 1:
+    #    events.extend(event_queues[0])
     return events
